2a06069c756b6309a43a17d764f07ed5ad9a87e2
marcus gray <abundanceisfree@gmail.com>
2016-05-24 17:09:45 -0700

    scraping2

--- www/js/scraping.js
+++ www/js/scraping.js
@@ -6,8 +6,8 @@ var target = "https://www.reverbnation.com/1875127/shows?class=prev&page=1&past=
 request(target, function(err, response, body){
     if(!err && response.statusCode == 200){
         $ = cheerio.load(body);
-        $("article.profile_backpage_shows_container", "#page_contents").each(function(article){
-          console.log(article);
+        $("class.profile_backpage_shows_row").each(function(class){
+          console.log(class);
 
         });
     }
